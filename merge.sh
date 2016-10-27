35bb15912072e917d1a488fa6dfe471d0a357a29
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-28 00:42:39 +0530

    Initial commit
    merge.sh, shell script to --/merge/-- whole "d3-alfred-nobel®" in to "NobelDB®" in one go.
    Usage: $ merge.sh

---
+++ merge.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+# =========================== I'm Prabhat® ===========================
+#               Technical Architect & Data Scientist
+#                           d3-alfred-nobel®
+#                               ~~~~~~~~
+# Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ====================================================================
