a1caee1cf08870e24243baa4ad7f3f795b6ba925
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-28 02:49:02 +0530

    Initial commit
    Index module of d3-alfred-nobel®.

---
+++ core/src/index.js
@@ -0,0 +1,14 @@
+/*!
+ * d3-alfred-nobel®
+ * ----------------
+ * for NobelDB® — about Alfred Nobel.
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * ___________________________________________________________________________
+ *
+ * @version   : 1.0.0
+ * @license   : MIT License
+ * @require   : d3.js®
+ * @build     : SEED™ — Stockholm
+ *              |---- A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ */
