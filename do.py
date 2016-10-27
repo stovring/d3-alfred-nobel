e1a77c38d2bb9655458a5b0b65520ed59e17a754
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-28 01:00:00 +0530

    Initial commit
    Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.

--- do.py
+++ do.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# ------------------------------------------------------------------------
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# Licensed under the MIT License (the "License");
+# you may not use this file except in compliance with the License.
+# ========================================================================
+#  ,-.
+#  ,     ,-.   ,-.
+# / \   (   )-(   )
+# \ |  ,.>-( * )-<
+#  \|,' (   )-(   )
+#   Y ___`-'   `-'
+#   |/__/   `-'
+#   |
+#   |
+#   |  -~PRABHAT~-
+# __|_____________
