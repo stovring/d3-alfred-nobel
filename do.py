931f0a193b34e7b3bddc57ec3911259ff314c87c
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-28 02:28:01 +0530

    Initial commit.🇸🇪
    Header of d3-alfred-nobel® | Build: SEED™ — Stockholm.

--- do.py
+++ do.py
@@ -37,3 +37,34 @@ import tempfile
 import warnings
 
 localtime = time.asctime(time.localtime(time.time()))
+header ='''
+/*!
+ * d3-alfred-nobel®
+ * ----------------
+ * Using a Capacity Constrained Point Distribution to display
+ * a picture of Alfred Nobel — founder of Nobel Foundation.
+ * This is a variant of the original algorithm by Michael Balzer,
+ * Thomas Schlömer & Oliver Deussen (University of Konstanz, Germany, 2009),
+ * in which I use a Voronoi Diagram to create a topology of the current sites,
+ * and only swap the points between neighbouring sites
+ * (and neighbours of neighbours).
+ * It appears to be much faster than the original algorithm.
+ * ___________________________________________________________________________
+ *
+ * NobelDB®: A Manually Curated Dedicated Database of Nobel Laureates.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 28-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : d3.js®
+ * @build     : SEED™ — Stockholm
+ *              |---- A Sequømics Product.
+ * ___________________________________________________________________________
+ */
+'''
