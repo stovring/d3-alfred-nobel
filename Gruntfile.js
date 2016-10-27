9c8665b55eb8d174c240adf89b7ff69639aac591
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-28 00:50:18 +0530

    Initial commit.💋
    --/The Heart of Build System/-- of "d3-alfred-nobel®".

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,26 @@
 #!/usr/bin/env node
+
+
+/*!
+ * d3-alfred-nobel®
+ * NobelDB®: A Manually Curated Dedicated Database of Nobel Laureates.
+ * ____________________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 28-Oct-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @build     : SEED™
+ *              |---- A Sequømics Product.
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "d3-alfred-nobel®".
+ * ____________________________________________________________________
+ */
