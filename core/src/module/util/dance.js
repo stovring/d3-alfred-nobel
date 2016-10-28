17a0f587fd52be2f01c19ba7c4c276d5c3ee2a5c
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-28 06:38:02 +0530

    Initial commit
    Dance/utility — module of d3-alfred-nobel®.

---
+++ core/src/module/util/dance.js
@@ -0,0 +1,9 @@
+/*!
+ * @file — dance
+ * @module — utility
+ * @private
+ * @version — 1.0.0
+ * @require — d3.js®
+ * @author — Prabhat Kumar <prabhat.genome@gmail.com> (http://prabhatkumar.org/)
+ * @license — MIT License
+ */
