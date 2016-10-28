f5b1fc2af5acb65c9f706fa48f636fdfa5c520f2
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-28 05:54:21 +0530

    Initial commit
    Matrix module of d3-alfred-nobel®.

---
+++ core/src/module/matrix.js
@@ -0,0 +1,8 @@
+/*!
+ * @file — matrix
+ * @private
+ * @version — 1.0.0
+ * @require — d3.js®
+ * @author — Prabhat Kumar <prabhat.genome@gmail.com> (http://prabhatkumar.org/)
+ * @license — MIT License
+ */
